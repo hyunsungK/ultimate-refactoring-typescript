e03f755b10843883de566384ea219dac973b872f
Daniel Ramos <danielramosacosta@hotmail.com>
2019-04-16 23:18:26 +0100

    Done shopping cart

---
+++ __test__/ShoppingCartShould.ts
@@ -0,0 +1,28 @@
+import { ShoppingCart } from '../src/implementationChange/ShoppingCart';
+
+describe('ShoppingCart', () => {
+  it('counts the number of products', () => {
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.add(10);
+
+    expect(shoppingCart.numberOfProducts()).toEqual(1);
+  });
+  it('calculates the total price', () => {
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.add(10);
+
+    expect(shoppingCart.calculateTotalPrice()).toEqual(10);
+  });
+  it('knows when the discount applicable', () => {
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.add(100);
+
+    expect(shoppingCart.hasDiscount()).toBe(true);
+  });
+  it('knows when is not possible to apply discount', () => {
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.add(99);
+
+    expect(shoppingCart.hasDiscount()).toBe(false);
+  });
+});
---
+++ src/implementationChange/ShoppingCart.ts
@@ -0,0 +1,24 @@
+export class ShoppingCart {
+  private price: number;
+
+  constructor() {
+    // @ts-ignore
+    this.price = null; // simulate Java behavior
+  }
+
+  public add(price: number) {
+    this.price = price;
+  }
+
+  public calculateTotalPrice(): number {
+    return this.price;
+  }
+
+  public hasDiscount(): boolean {
+    return this.price >= 100;
+  }
+
+  public numberOfProducts(): number {
+    return 1;
+  }
+}
