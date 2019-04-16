2a19837f87d744d7ba5452901350c4759068ab5c
Daniel Ramos <danielramosacosta@hotmail.com>
2019-04-16 23:13:01 +0100

    Done God

---
+++ __test__/GodShould.ts
@@ -0,0 +1,12 @@
+import { God } from "../src/moveResponsibility/God";
+
+describe('God', () => {
+    it('adds numbers', () => {
+        const god = new God();
+        expect(god.add(2,2)).toEqual(4);
+    })
+    it('subtracts numbers', () => {
+        const god = new God();
+        expect(god.substract(2,2)).toEqual(0);
+    })
+})
---
+++ src/moveResponsibility/God.ts
@@ -0,0 +1,17 @@
+export class God {
+  public add(a: number, b: number): number {
+    return a + b;
+  }
+
+  public substract(a: number, b: number): number {
+    return a - b;
+  }
+
+  public sayHello() {
+    console.log('Hello!');
+  }
+
+  public sayBye() {
+    console.log('Good bye!');
+  }
+}
