24520c85203342cd80feac7c3909d3e06c4d9555
Daniel Ramos <danielramosacosta@hotmail.com>
2019-04-16 23:28:05 +0100

    Done ExternalCartClient

---
+++ __test__/ExternalCartClientTest.ts
@@ -0,0 +1,9 @@
+import { ExternalCartClient } from "../src/implementationChange/ExternalCartClient";
+
+describe('ExternalCartClient', () => {
+  it('formats the total price message', () => {
+    const client = new ExternalCartClient();
+
+    expect(client.formattedTotalPrice(50)).toEqual('Total price is 50 euro');
+  });
+});
---
+++ src/implementationChange/ExternalCartClient.ts
@@ -0,0 +1,10 @@
+import { ShoppingCart } from './ShoppingCart';
+
+export class ExternalCartClient {
+  public formattedTotalPrice(price: number): string {
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.add(price);
+
+    return `Total price is ${shoppingCart.calculateTotalPrice()} euro`;
+  }
+}
